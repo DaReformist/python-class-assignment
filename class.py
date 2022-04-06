f9596abe9ea0bfde751fb0dae90ca6afde7e2596
DaReformist <doephraim@gmail.comt>
2022-04-06 23:00:33 +0000

    initial

---
+++ class.py
@@ -0,0 +1,27 @@
+class KitchenStaff:
+    first_name=''
+    last_name=''
+    staff_id=''
+    rank=''
+
+
+class Staff:
+    first_name=''
+    last_name=''
+    department=''
+    staff_id=''
+    rank=''
+
+
+class Menu:
+    name=''
+    type_of_food=''
+    vegan=''
+
+
+class Commodity_for_sale:
+    drinks=''
+    packs=''
+    water=''
+    extra_portion=''
+
